8aa9f42e3659d1e951a6406c893fa05671975519
sofiamejiamuro <sofiamejiamuro@gmail.com>
2020-10-18 17:05:32 -0500

    useRef real use case

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,12 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const useFetch = (url) => {
-  
+  // su estas es true porque sí se renderea la primera vez 
+  // mantener la referencia cuando el componente siga montado, mantener la referencia al valor
+
+  const isMounted = useRef(true);
+
   const [state, setState] = useState({
     data:null,
     loading:true,
     error:null
   });
+
+  useEffect(() => {
+    // No quiero que haga nada
+
+    // Sólo que actúe cuando el compoenente se desmonte , that is the return 
+    return () => {
+      // Entonces si se desmonta el componente cambia a false,
+      isMounted.current = false;
+    }
+  }, [])
 
   useEffect(() => {
     
@@ -16,14 +30,30 @@ const useFetch = (url) => {
       error:null
     });
     
+    // qué pasa si la petición tarda mucho o si se cierra el componente? , ya no se ejecuta el setState, nos disparará un error
+    // para emular el error usamos un settimeout(), si mostramos el  'quote' usando el botón, comienza el loading,
+    // ocultamos antes de que termine el settimeout entonces no pemitimos quee se termine de renderear o montar el compoenente, se queda a medias
+    // error: Can't perform a React state update on an unmounted component.
+    // useRef nos resuelve este problema
+
     fetch(url)
       .then( resp => resp.json() )
       .then( data => {
-        setState({
-          data: data,
-          loading: false,
-          error: null
-        })
+
+        setTimeout(() => {
+          // Sólo funcionará si isMounted es true
+          if (isMounted.current){
+            setState({
+              data: data,
+              loading: false,
+              error: null
+            })
+          } else {
+            console.log('setState no se llamó');
+          }
+          
+        }, 4000);
+       
       });
     
   }, [url]);
